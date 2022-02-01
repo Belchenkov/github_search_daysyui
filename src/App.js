02d26b13efe6aca9ef8f5ecc75e6b3b1a24f4c1a
Алексей Бельченков <u608110@gmail.com>
2022-02-01 10:23:04 +0400

    Pages & Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import {
     BrowserRouter as Router,
+    Routes,
     Route,
 } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -13,7 +17,12 @@ function App() {
           <Navbar />
 
           <main className='container mx-auto px-3 pb-12'>
-              Content
+              <Routes>
+                  <Route path='/' element={<Home />} exact />
+                  <Route path='/about' element={<About />} exact />
+                  <Route path='/notfound' element={<NotFound />} />
+                  <Route path='/*' element={<NotFound />} />
+              </Routes>
           </main>
 
           <Footer />
