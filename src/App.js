daf321879b15f876a245d73f4a915499a8d9baa2
Алексей Бельченков <u608110@gmail.com>
2022-02-01 10:06:30 +0400

    Footer Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
 } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
@@ -11,9 +12,11 @@ function App() {
       <div className="flex flex-col justify-between h-screen">
           <Navbar />
 
-          <main>
+          <main className='container mx-auto px-3 pb-12'>
               Content
           </main>
+
+          <Footer />
       </div>
     </Router>
   );
