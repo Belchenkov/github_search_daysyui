76404ff619b3f2aa0e078af9facedb5ae8dc83f4
Алексей Бельченков <u608110@gmail.com>
2022-02-15 09:54:23 +0400

    User Search Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
+
 import UserResults from '../components/users/UserResults';
+import UserSearch from '../components/users/UserSearch';
 
 const Home = () => {
     return (
         <>
+            <UserSearch />
             <UserResults />
         </>
     );
