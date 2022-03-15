2e70384cc44c7655bbf6d92527b2982c9deb1fca
Алексей Бельченков <u608110@gmail.com>
2022-03-15 15:42:41 +0400

    Fix Prepare deploy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import UserResults from '../components/users/UserResults';
 import UserSearch from '../components/users/UserSearch';
